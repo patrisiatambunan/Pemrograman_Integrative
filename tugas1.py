cc7709b5be15bee1790fe929aa60a1d2a19bb5fc
patrisiatambunan <80022014+patrisiatambunan@users.noreply.github.com>
2021-03-04 16:05:15 +0700

    Create tugas1.py

---
+++ tugas1.py
@@ -0,0 +1,11 @@
+angka = int(input("Masukkan angka: "))
+
+if angka > (0) :
+	print("Angka yang anda masukkan bernilai positif")
+
+elif angka == (0) :
+	print("Angka yang anda masukkan bernilai 0")
+
+else:
+	print("Angka yang anda masukkan bernilai negatif")
+
