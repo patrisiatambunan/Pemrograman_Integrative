50aac3f65af87d6e88a74348bfc906e3a3d94972
patrisiatambunan <80022014+patrisiatambunan@users.noreply.github.com>
2021-03-04 16:06:27 +0700

    perubahan sehingga hanya bisa memasukkan angka

--- tugas1.py
+++ tugas1.py
@@ -1,11 +1,15 @@
-angka = int(input("Masukkan angka: "))
-
-if angka > (0) :
-	print("Angka yang anda masukkan bernilai positif")
-
-elif angka == (0) :
-	print("Angka yang anda masukkan bernilai 0")
-
-else:
-	print("Angka yang anda masukkan bernilai negatif")
-
+while True:
+  try:
+    angka = int(input("Masukkan angka: "))
+    if angka >= 0 or angka < 0:
+        if angka >= 0:
+            if angka == 0:
+                print("Angka yang anda masukkan bernilai 0")
+            else:
+                print("Angka yang anda masukkan bernilai positif")
+        else:
+            print("Angka yang anda masukkan bernilai negatif")
+        break
+    print("Anda hanya boleh memasukkan angka")
+  except Exception as e:
+    print("Anda hanya boleh memasukkan angka")
